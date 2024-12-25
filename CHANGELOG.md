968824e04dcc7ccefb2681436841a1a754b33f0e
Frank Hunleth <fhunleth@troodon-software.com>
2024-12-25 08:12:03 -0600

    Add package metadata and docs

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## v0.1.0
+
+Initial release. Benchmarks should all compile and run on Elixir 1.18 and
+Erlang/OTP 27.
--- mix.exs
+++ mix.exs
@@ -2,20 +2,28 @@ defmodule NervesBench.MixProject do
   use Mix.Project
 
   @version "0.1.0"
-  # @source_url "https://github.com/fhunleth/beam_benchmarks"
+  @source_url "https://github.com/fhunleth/beam_benchmarks"
 
   def project do
     [
       app: :beam_benchmarks,
       version: @version,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       compilers: [:elixir_make | Mix.compilers()],
+      description: description(),
+      package: package(),
+      docs: docs(),
       make_targets: ["all"],
       make_clean: ["mix_clean"],
       make_error_message: "",
-      deps: deps()
+      deps: deps(),
+      preferred_cli_env: %{
+        docs: :docs,
+        "hex.publish": :docs,
+        "hex.build": :docs
+      }
     ]
   end
 
@@ -27,7 +35,30 @@ defmodule NervesBench.MixProject do
 
   defp deps do
     [
-      {:elixir_make, "~> 0.6", runtime: false}
+      {:elixir_make, "~> 0.6", runtime: false},
+      {:ex_doc, "~> 0.26", only: :docs, runtime: false}
+    ]
+  end
+
+  defp description do
+    "Various BEAM benchmarks in one place"
+  end
+
+  defp package do
+    [
+      # See source files for licenses
+      licenses: ["ErlPL-1.1", "Apache-2.0"],
+      links: %{"Github" => @source_url}
+    ]
+  end
+
+  defp docs do
+    [
+      extras: ["README.md", "CHANGELOG.md"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url,
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
     ]
   end
 end
